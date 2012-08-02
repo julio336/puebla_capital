eaac5fbd8ec6ef23c888655ae64185c32557b7ea
julio <julio336@hotmail.com>
2012-08-02 13:47:24 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PueblaCapital::Application.config.secret_token = '5e58d059caab6e60d77ad20a60aca7f9e7664e366ede762661a20857386681e4cbbeb5f1540024c0b3c47a1adc3c6a70b4831d52979ea94db2fa1cebe44bba85'
